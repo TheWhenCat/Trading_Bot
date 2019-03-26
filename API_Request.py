fe84a4424c8b5b321bdd1e5d020a81be9144cf19
Dhruva Venkat <dhruva735@gmail.com>
2019-03-25 23:04:49 -0400

    Oops

--- API_Request.py
+++ API_Request.py
@@ -7,7 +7,7 @@ import pandas
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 
-ts = TimeSeries(key='KE1NVXP9LYFO1Y5W', output_format='pandas')
+ts = TimeSeries(key='', output_format='pandas')
 
 data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
 data['2. high'].plot()
