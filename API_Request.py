aa3f739bf6be3d421f5dfdd356e07161a92b0186
Dhruva Venkat <dhruva735@gmail.com>
2019-03-26 20:47:05 -0400

    Just started working on a generalized API for my bot
    Basing the entire API off of the AlphaVantage Wrapper that I initially
    planned to use, am retrofitting it

--- API_Request.py
+++ API_Request.py
@@ -1,6 +1,5 @@
 import requests
-import time
-from pprint import pprint
+import os
 import matplotlib.pyplot as plt
 import pandas
 from io import StringIO
@@ -16,8 +15,30 @@ class request_builder(object):
 
     ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query?"
 
-    def __init__(self):
-        request = ALPHA_VANTAGE_API_URL + 'function=' + function
+    def __init__(self, key=None, output_format='pandas', *args, **kwargs):
+
+        if key is None:
+            key = os.getenv('ALPHAVANTAGE_API_KEY')
+        if not key or not isinstance(key, str):
+            raise ValueError("No key supplied, please supply one")
+
+        self.key = key
+        self.output_format = output_format
+
+        
+
+
+
+
+
+    def Stock_Time_Series(self):
+
+    def Foreign_Exchange(self):
+
+    def Digital_Crypto_Currency(self):
+
+    def Technical_Indicators(self):
+
 
 
 r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=KE1NVXP9LYFO1Y5W&datatype=csv")
