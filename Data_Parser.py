49b3b6e6bef680f24eb31a8592ad310bc8be6f9c
TheWhenCat <dhruva735@gmail.com>
2019-04-13 20:44:18 -0400

    Desktop Switch

--- Data_Parser.py
+++ Data_Parser.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import inspect
-import scikit-learn
+# import scikit
 import tensorflow
 
 
@@ -20,9 +20,24 @@ class Parser(request_builder):
 
     def plotter(self, data):
 
+        parsed_time = {}
+        for idx, time in enumerate(data['timestamp']):
+                date, hour = time.split(' ')
+                parsed_time[idx] = hour
+        # print(parsed_time)
+        for row in data:
+            if row == 'timestamp':
+                pass
+            else:
+                data_dict = {}
+                for idx, value in enumerate(data[row]):
+                    data_dict[idx] = value
+
+                for idx in data_dict:
+                    plt.plot(parsed_time[idx], data_dict[idx])
+                    plt.show()
+
 
 test = Parser(key = apikey)
 data = test.api_call(symbol='GOOGL', function='TIME_SERIES_INTRADAY', interval='30min', datatype="csv")
-
-plt.plot(data["close"])
-plt.show()
+test.plotter(data)
